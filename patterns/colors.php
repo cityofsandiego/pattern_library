1ad213c7370c1c503d9b693b9ac54b8f7d375731
Ron Vazquez <ron-vazquez@users.noreply.github.com>
2017-10-07 15:48:31 -0700

    added available color variable names and website-only colors

--- patterns/colors.php
+++ patterns/colors.php
@@ -3,6 +3,8 @@
 require_once( '../config.php' );
 require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 
+<!-- color variables are in _settings.scss -->
+
         <div class="l-constrained row patterns background-white l-padding-mobile-hd">
             <aside>
                 <div class="three columns">
@@ -33,29 +35,29 @@ require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 <div class="row">
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#0098db;"></i></p>
-		<p>#0098db<br>rgb (0, 152, 219)</p>
+		<p>#0098db<br>rgb (0, 152, 219)<br><span class="fine-print">cerulean</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#00c7b2;"></i></p>
-		<p>#00c7b2<br>rgb (0, 199, 178)</p>
+		<p>#00c7b2<br>rgb (0, 199, 178)<br><span class="fine-print">robin</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#ffa02f;"></i></p>
-		<p>#ffa02f<br>rgb (255, 158, 23)</p>
+		<p>#ffa02f<br>rgb (255, 158, 23)<br><span class="fine-print">sunshade</span></p>
 	</div>
 </div>
 <div class="row">
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#fcd900;"></i></p>
-		<p>#fcd900<br>rgb (252, 217, 0)</p>
+		<p>#fcd900<br>rgb (252, 217, 0)<br><span class="fine-print">schoolbus</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#37424a;"></i></p>
-		<p>#37424a<br>rgb (55, 66, 74)</p>
+		<p>#37424a<br>rgb (55, 66, 74)<br><span class="fine-print">spruce</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#dad7cb;"></i></p>
-		<p>#dad7cb<br>rgb (218, 215, 203)</p>
+		<p>#dad7cb<br>rgb (218, 215, 203)<br><span class="fine-print">westar</span></p>
 	</div>
 </div>                 
 
@@ -75,7 +77,7 @@ require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#005bbb;"></i></p>
-		<p>#005bbb<br>rgb (0, 91, 187)</p>
+		<p>#005bbb<br>rgb (0, 91, 187)<br><span class="fine-print">science</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#00549f;"></i></p>
@@ -117,7 +119,7 @@ require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#fed100;"></i></p>
-		<p>#fed100<br>rgb (254, 209, 0)</p>
+		<p>#fed100<br>rgb (254, 209, 0)<br><span class="fine-print">gold</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#d7a900;"></i></p>
@@ -159,7 +161,7 @@ require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#5d676f;"></i></p>
-		<p>#5d676f<br>rgb (94, 106, 113)</p>
+		<p>#5d676f<br>rgb (94, 106, 113)<br><span class="fine-print">shuttle</span></p>
 	</div>
 	<div class="four columns text-center l-margin-bm">
 		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#1b242a;"></i></p>
@@ -169,7 +171,85 @@ require_once( ABSPATH . PARTIALS . '/header.php' ); ?>
 					
                 </div>
 
-                
+                <div id="website" class="l-container">
+                    <div class="row l-margin-bm">
+                        <div class="twelve columns">
+                            <h2>Website-only Colors</h2>
+                        </div>
+                    </div>
+                    
+<!--<div class="row">
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#f6f6f6;"></i></p>
+		<p>#f6f6f6<br>rgb (246, 246, 246)<br><span class="fine-print">off-white </span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#e4e9eb;"></i></p>
+		<p>#e4e9eb<br>rgb (228, 233, 235)<br><span class="fine-print">mercury</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#dde2e5;"></i></p>
+		<p>#dde2e5<br>rgb (221, 226, 229)<br><span class="fine-print">alto</span></p>
+	</div>
+</div>
+<div class="row">
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#656667;"></i></p>
+		<p>#656667<br>rgb (101, 102, 103)<br><span class="fine-print">gray</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#333332;"></i></p>
+		<p>#333332<br>rgb (51, 51, 50)<br><span class="fine-print">charcoal</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#222222;"></i></p>
+		<p>#222222<br>rgb (34, 34, 34)<br><span class="fine-print">off-black </span></p>
+	</div>
+</div>  
+<div class="row">
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#e89e3b;"></i></p>
+		<p>#e89e3b<br>rgb (232, 158, 59)<br><span class="fine-print">tulip</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#ee3228;"></i></p>
+		<p>#ee3228<br>rgb (238, 50, 40)<br><span class="fine-print">pomegranate</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#0065b2;"></i></p>
+		<p>#0065b2<br>rgb (0, 101, 178)<br><span class="fine-print">endeavour</span></p>
+	</div>
+</div>  -->
+<div class="row">
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#f6f6f6;"></i></p>
+		<p>#f6f6f6<br>rgb (246, 246, 246)<br><span class="fine-print">off-white </span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#e89e3b;"></i></p>
+		<p>#e89e3b<br>rgb (232, 158, 59)<br><span class="fine-print">tulip</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#0065b2;"></i></p>
+		<p>#0065b2<br>rgb (0, 101, 178)<br><span class="fine-print">endeavour</span></p>
+	</div>
+</div>    
+<div class="row">
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#659f13;"></i></p>
+		<p>#659f13<br>rgb (101, 159, 19)<br><span class="fine-print">success</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#e28327;"></i></p>
+		<p>#e28327<br>rgb (226, 131, 39)<br><span class="fine-print">warning</span></p>
+	</div>
+	<div class="four columns text-center l-margin-bm">
+		<p><i class="icon--xl icon--circle" style="padding:1.5em; background-color:#c51313;"></i></p>
+		<p>#c51313<br>rgb (197, 19, 19)<br><span class="fine-print">error</span></p>
+	</div>
+</div>     
+
+                </div>
 
             </div>
         </div>
